d066b0669701c73fe1792b9734c5fafbece252bc
Zbigniew013 <zbigniew.szalkowski@gmail.com>
2022-07-03 13:48:23 +0200

    add store

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,14 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Column = props => {
+
+  const cards = useSelector(state => 
+    state.cards.filter(card => 
+      card.columnId === props.id));
+
 
   return (
     <article className={styles.column}>
@@ -11,7 +17,7 @@ const Column = props => {
         {props.title}
       </h2>
       <ul className={styles.cards}>
-        {props.cards.map(card => 
+        {cards.map(card => 
         <Card 
           key={card.id} 
           title={card.title} />)}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'font-awesome/css/font-awesome.min.css';
 
-
 import './styles/normalize.scss';
 import './styles/global.scss'; 
+
+import { Provider } from 'react-redux';
+import store from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
